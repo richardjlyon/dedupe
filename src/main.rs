6776203a09e289b21ff36b83c750bce6c94c4c47
Richard Lyon <richlyon@mac.com>
2023-02-19 19:45:54 +0000

    start date_time()

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,86 @@
-use walkdir::{WalkDir, DirEntry};
-use exif::{DateTime, In, Reader, Value, Tag, Exif};
-use std::ffi::OsStr;
+use exif::{DateTime, Exif, In, Reader, Tag, Value};
+use indicatif::ProgressBar;
 use std::fs::File;
 use std::io::BufReader;
-use std::path::PathBuf;
-use indicatif::ProgressBar;
+use std::path::{Component, PathBuf};
+use walkdir::WalkDir;
 
 struct Image {
     filepath: PathBuf,
     exif: Exif,
+    is_duplicate: bool,
+}
+
+impl Image {
+    pub fn new(filepath: PathBuf, exif: Exif) -> Self {
+        Self {
+            filepath,
+            exif,
+            is_duplicate: false,
+        }
+    }
+
+    pub fn date_time(&self) {
+        if let Some(field) = self.exif.get_field(Tag::DateTime, In::PRIMARY) {
+            match field.value {
+                Value::Ascii(ref vec) if !vec.is_empty() => {
+                    if let Ok(datetime) = DateTime::from_ascii(&vec[0]) {
+                        println!("Year of DateTime is {}.", datetime.year);
+                    }
+                }
+                _ => {}
+            }
+        }
+    }
 }
 
 fn main() {
-
     // let root = "/Volumes/home/Photos/MobileBackup";
     let root = "/Users/richardlyon/Dev/rust/dedupe/images";
-    let mut filepaths:Vec<PathBuf> = Vec::new();
-    let mut images:Vec<Image> = Vec::new();  
-    
+    let mut filepaths: Vec<PathBuf> = Vec::new();
+    let mut backup_images: Vec<Image> = Vec::new();
+    let mut library_images: Vec<Image> = Vec::new();
+
     // generate recursive list of images
     println!("Getting images from {} ...", root);
     for entry in WalkDir::new(root) {
         // get image files, assumed to be a result with an extension
         let entry = entry.unwrap();
-        let filepath = entry.into_path(); 
-
+        let filepath = entry.into_path();
         if filepath.extension().is_some() {
             filepaths.push(filepath)
-        } 
+        }
     }
-    
-    // get exif data
-    let number:u64 =  filepaths.len() as u64;
+
+    // get exif data for 'backup' and 'library' images
+    let number: u64 = filepaths.len() as u64;
     let bar = ProgressBar::new(number);
     for filepath in filepaths {
         let file = File::open(&filepath).unwrap();
-        let exif = Reader::new()
-            .read_from_container(&mut BufReader::new(&file));
+        let exif = Reader::new().read_from_container(&mut BufReader::new(&file));
         if exif.is_ok() {
-            images.push(Image{filepath:filepath, exif:exif.unwrap()})
+            // get the library it belongs to, which is the 3rd last path component
+            let library = match filepath.components().nth_back(3).unwrap() {
+                Component::Normal(lib) => lib,
+                _ => continue,
+            };
+            // store it in the corresponding vector
+            match library.to_str().unwrap() {
+                "MobileBackup" => backup_images.push(Image::new(filepath, exif.unwrap())),
+                "PhotoLibrary" => library_images.push(Image::new(filepath, exif.unwrap())),
+                _ => {}
+            }
+
+            bar.inc(1);
         }
-        bar.inc(1);
+        bar.finish();
+
+        println!("Found {} in backup", backup_images.len());
+        println!("Found {} in library", library_images.len());
+
+        // detect duplicates
+        for library_image in &library_images {
+            library_image.date_time();
+        }
     }
-    bar.finish()
-
-    //         Err(_) => continue,
-    //         Ok(exif) => images.push(Image{filepath: filepath, exif: exif})
-    //     };
-    //     print!(".");
-    // }
-
-
-    // parse exif data
-    // println!("Analysing images...");
-    // for image in images {
-
-    //     let file = File::open(&image.filepath).unwrap();
-
-    //     let exif = match Reader::new()
-    //         .read_from_container(&mut BufReader::new(&file)) {
-    //         Err(_) => continue,
-    //         Ok(e) => e
-    //     };
-            
-    //     if let Some(field) = exif.get_field(Tag::PixelXDimension, In::PRIMARY){
-    //             if let Some(width) = field.value.get_uint(0) {
-    //                     println!("Valid width of the image is {}.", width);
-    //                 }
-    //             }
-    //         }
-    
-
-    // detect and mark duplicates for deletion
-
-    // delete duplicates
-
 }
